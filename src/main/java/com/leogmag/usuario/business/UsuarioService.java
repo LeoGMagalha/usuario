b52b718a9cbbf697800201effd3f4e4f077f0ee0
LeoGMagalha <leosmagalha@gmail.com>
2026-02-12 21:05:08 -0300

    finalizando post

--- src/main/java/com/leogmag/usuario/business/UsuarioService.java
+++ src/main/java/com/leogmag/usuario/business/UsuarioService.java
@@ -3,8 +3,11 @@ package com.leogmag.usuario.business;
 import com.leogmag.usuario.business.converter.UsuarioConverter;
 import com.leogmag.usuario.business.dto.UsuarioDTO;
 import com.leogmag.usuario.infrastructure.entity.Usuario;
+import com.leogmag.usuario.infrastructure.exceptions.ConflictException;
 import com.leogmag.usuario.infrastructure.repository.UsuarioRepository;
 import lombok.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
@@ -13,11 +16,30 @@ import org.springframework.stereotype.Service;
 public class UsuarioService {
     private final UsuarioRepository usuarioRepository;
     private final UsuarioConverter usuarioConverter;
+    private final PasswordEncoder passwordEncoder;
 
     public UsuarioDTO salvaUsuario(UsuarioDTO usuarioDTO){
+
+        emailExiste(usuarioDTO.getEmail());
+        usuarioDTO.setSenha(passwordEncoder.encode(usuarioDTO.getSenha()));
+
         Usuario usuarioEntity = usuarioConverter.paraUsuario(usuarioDTO);
         return usuarioConverter.paraUsuarioDTO(
                 usuarioRepository.save(usuarioEntity)
         );
+    }
+
+    public void emailExiste(String email){
+        try{
+            boolean existe = verificaEmailExistente(email);
+            if(existe){
+                throw new ConflictException("Email já cadastrado" + email);
+            }
+        } catch (ConflictException e){
+            throw new ConflictException("Email já cadastrado" + e.getCause());
+        }
+    }
+    public boolean verificaEmailExistente(String email){
+        return usuarioRepository.existsByEmail(email);
     }
 }
